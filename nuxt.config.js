196d4a949a2e9ac4978271a9dc51d9df9bd5fa46
Eden Gomez <gresseden@gmail.com>
2023-03-11 13:50:13 -0500

    adding serveless

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
     // generate: {
     //     dir: "public",
     // },
-    // target: "serverless",
+    target: "serverless",
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
