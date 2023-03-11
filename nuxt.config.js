76aa2e699d553359a71dcc7cb2ba4bacd32b30c2
Eden Gomez <gresseden@gmail.com>
2023-03-11 14:13:38 -0500

    adding now.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
     // generate: {
     //     dir: "public",
     // },
-    target: "serverless",
+    target: "static",
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
